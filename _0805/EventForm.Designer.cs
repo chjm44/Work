6ad79b325775956419a5b603f473f73e4ea44dca
chjm44 <chc807000@naver.com>
2024-08-05 20:46:41 +0900

    s

---
+++ _0805/EventForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace _0805
+{
+    partial class EventForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txdate = new System.Windows.Forms.TextBox();
+            this.txevent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txdate
+            // 
+            this.txdate.Enabled = false;
+            this.txdate.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txdate.Location = new System.Drawing.Point(50, 38);
+            this.txdate.Name = "txdate";
+            this.txdate.Size = new System.Drawing.Size(274, 26);
+            this.txdate.TabIndex = 0;
+            // 
+            // txevent
+            // 
+            this.txevent.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txevent.Location = new System.Drawing.Point(50, 113);
+            this.txevent.Name = "txevent";
+            this.txevent.Size = new System.Drawing.Size(274, 26);
+            this.txevent.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Event";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(249, 172);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // EventForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 228);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txevent);
+            this.Controls.Add(this.txdate);
+            this.Name = "EventForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EventForm";
+            this.Load += new System.EventHandler(this.EventForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txdate;
+        private System.Windows.Forms.TextBox txevent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ _0805/EventForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0805
+{
+    public partial class EventForm : Form
+    {
+        //create a connectionstring
+        String connString = "server=lovalhost;user id=root;darabase=db_calendar;sslmode=none";
+        //I already created a database using xampp
+
+        public EventForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EventForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
